a1052c2e8852516fee5e1283d9fb5b0c9eb68f37
zenmeraki7 <zenmeraki7>
2025-06-19 11:51:47 +0530

    till table

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import SignUp from "./pages/Auth/SignUp"
 import ResetPassword from "./pages/Auth/ResetPass"
 import Forgotpassword from "./pages/Auth/Forgotpassword"
+import ReferalTable from "./pages/ReferalTable"
 function App() {
 
   return (
@@ -14,6 +15,7 @@ function App() {
     <Route path="/sign-up" element={<SignUp />} />
    <Route path="/reset-password" element={<ResetPassword/>}/>
     <Route path="/forgot-password" element={<Forgotpassword />} />
+    <Route path="/referal" element={<ReferalTable />} />
 
   </Routes>
    </>
